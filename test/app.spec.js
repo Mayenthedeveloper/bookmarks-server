1141b5afba31e62676ff0633a362c19a7acc2cfb
Mayen Akpan <neroxma@yahoo.com>
2020-12-11 23:40:52 -0800

    Initial commit

---
+++ test/app.spec.js
@@ -0,0 +1,11 @@
+
+const app = require('../src/app')
+const store = require('../src/store')
+
+describe('App', () => {
+  it('GET / responds with 200 containing "Hello, world!"', () => {
+    return supertest(app)
+      .get('/')
+      .expect(200, 'Hello, world!')
+  })
+})
